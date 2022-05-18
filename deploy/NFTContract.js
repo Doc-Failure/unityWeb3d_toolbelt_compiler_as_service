c6d0deea8da8a63a4f79851c1a50c7ebe4ffc79f
doc_failure <stefano.convertino@gmail.com>
2022-05-18 12:02:09 +0200

    modified deploy script

--- deploy/NFTContract.js
+++ deploy/NFTContract.js
@@ -10,23 +10,23 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
   var Fee=1;
 
   const fs = require('fs');
-  fs.readFile('Networks_ChainlinkVFR_Adresses.json', async (err, data) => {
-    const networks = JSON.parse(data);
-    const network = networks.filter((elem)=>{ 
-      if(elem.ChainId== process.env.NETWORK_ID)
-        return elem
-      });
-    VRF_Coordinator=network[0].VRF_Coordinator;
-    LINK_Token=network[0].LINK_Token;
-    Key_Hash=network[0].Key_Hash;
-    Fee=network[0].Fee;
-    
-    let { address } = await deploy("NFTContract", {
-      from: deployer,
-      args: [VRF_Coordinator, LINK_Token, Key_Hash, Fee],
-      log: true,
-      deterministicDeployment: false
+  const networkInFile = fs.readFileSync('Networks_ChainlinkVFR_Adresses.json');
+
+  const networks = JSON.parse(networkInFile);
+  const network = networks.filter((elem)=>{ 
+    if(elem.ChainId== process.env.NETWORK_ID)
+      return elem
     });
+  VRF_Coordinator=network[0].VRF_Coordinator;
+  LINK_Token=network[0].LINK_Token;
+  Key_Hash=network[0].Key_Hash;
+  Fee=network[0].Fee;
+
+  let { address } = await deploy("NFTContract", {
+    from: deployer,
+    args: [VRF_Coordinator, LINK_Token, Key_Hash, Fee],
+    log: true,
+    deterministicDeployment: false
   });
 }
 
